5c75d43911912291795de35a0738efc847e35d31
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2022-09-07 15:46:45 +0500

    s

---
+++ constant.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color yellowColor = Colors.yellow;
+const Color blueColor = Colors.blue;
+const headingTextStyle = TextStyle(
+  fontFamily: "Horizon",
+  fontSize: 35,
+  color: yellowColor,
+);
